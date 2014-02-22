3b90e218c36861f258921b9421545c35d3d9034b
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-02-22 01:10:45 -0800

    expanded submit detils

--- collections/posts.js
+++ collections/posts.js
@@ -5,4 +5,37 @@ Posts.allow({
 		//only allow posting if logged in
 		return !! userId;
 	}
+});
+
+Meteor.methods({
+	post: function(postAttributes) {
+		var user = Meteor.user(), 
+			postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+		// ensure user logged in
+		if (!user)
+			throw new Meteor.Error(401, "You need to login to post new stories");
+
+		// ensure post has a title
+		if (!postAttributes.title)
+			throw new Meteor.Error(422, "Please fill in a headline");
+
+		// check no previous posts with same link
+		if (postAttributes.url && postWithSameLink) {
+			throw new Meteor.Error(302,
+				'This link has already been posted',
+				postWithSameLink.id_);
+		}
+
+		// pick out the whitelisted keys
+		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'),{
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+
+		var postId = Posts.insert(post);
+
+		return postId;
+	}
 })
